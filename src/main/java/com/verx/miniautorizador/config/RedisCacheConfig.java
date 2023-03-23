9dfc7fc94fa286273d011746ccef2fff53cb08bc
Inael Rodrigues <inael.rodrigues@gmail.com>
2023-03-23 00:01:47 -0300

    adicionando suporte ao macos

--- src/main/java/com/verx/miniautorizador/config/RedisCacheConfig.java
+++ src/main/java/com/verx/miniautorizador/config/RedisCacheConfig.java
@@ -1,19 +1,6 @@
 package com.verx.miniautorizador.config;
 
-import com.verx.miniautorizador.core.model.Cartao;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
 
 
 public class RedisCacheConfig {
--- src/main/java/com/verx/miniautorizador/controller/TransacaoController.java
+++ src/main/java/com/verx/miniautorizador/controller/TransacaoController.java
@@ -3,12 +3,14 @@ package com.verx.miniautorizador.controller;
 import com.verx.miniautorizador.core.StatusTransacao;
 import com.verx.miniautorizador.core.dto.TransacaoCartaoDTO;
 import com.verx.miniautorizador.service.TransacaoService;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name="Mini-autorizador - Transação",description = "API de Autorização de Transações com Cartão")
 @RestController
